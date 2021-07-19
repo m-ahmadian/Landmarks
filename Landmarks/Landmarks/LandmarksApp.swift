2384972e81df538e18151f9d4638e5ecd50c77a4
Mehdad <mehrdad.ahmadian@gmail.com>
2021-07-19 00:40:04 -0400

    Create Xcode project

---
+++ Landmarks/Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Mehrdad Ahmanian on 2021-07-19.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
