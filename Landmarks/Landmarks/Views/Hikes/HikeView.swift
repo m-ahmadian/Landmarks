dcb433e5a69bfebef1fc0b3a6a3ddbc01520cc15
Mehdad <mehrdad.ahmadian@gmail.com>
2021-08-01 00:16:11 -0400

    Add Animations to Individual Views

--- Landmarks/Landmarks/Views/Hikes/HikeView.swift
+++ Landmarks/Landmarks/Views/Hikes/HikeView.swift
@@ -46,7 +46,6 @@ struct HikeView: View {
                         .rotationEffect(.degrees(showDetail ? 90 : 0))
                         .scaleEffect(showDetail ? 1.5 : 1)
                         .padding()
-                        .animation(.default)
                 }
             }
 
